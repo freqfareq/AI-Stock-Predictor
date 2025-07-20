3ebd82cc884b8883dc610ee296b564e889b025c5
freqfareq <fareqali13@gmail.com>
2025-07-20 17:15:58 +0530

    p

--- manipulation/creation.py
+++ manipulation/creation.py
@@ -1,27 +1,11 @@
 import pandas as pd
 
-rl = pd.read_excel("Data/dataset_noLag.xlsx",  header=0)
+rl = pd.read_excel(r"Data\reliance_data.xlsx",  header=0)
 
 def create():
     rl["Close"]= pd.to_numeric(rl["Close"], errors='coerce')
 
-    rl["20 SMA"]= rl["Close"].rolling(window=20).mean() # 20 SMA formula 
-    rl["SD"]= rl["Close"].rolling(window=20).std() # Standard deviation formula
-    rl["Upper Band"] = rl["20 SMA"] + (2 * rl["SD"]) # upper band formula
-    rl["Middle band"] =rl["20 SMA" ] # middle band formula 
-    rl["Lower Band"] = rl["20 SMA"] - (2 * rl["SD"]) # Lower band formula
-    rl["chng %"] = rl["Close"].pct_change() * 100 # close price percentage change
-    rl["volume %"] =rl["Volume"].pct_change() * 100 # Volume percentage change . 
-
-    rl["sma_20"]= rl['Close'].rolling(20).mean()
-    rl["price_vs_sma"]= (rl['Close'] - rl["sma_20"])/ rl["sma_20"] # sma deviation 
-
-    rl["percent_b"] =(rl['Close']- rl['Lower Band']) / (rl["Upper Band"]-rl["Lower Band"]) # B percentage (volatility) 
-
     rl["future_target_1wk"]= rl["Close"].shift(-1)/rl["Close"] - 1 # Return or Future Target (labelling)
-
-
-
 
 
 ####################################################
@@ -56,7 +40,7 @@ def calculate_rsi(series, period=14):
     rsi = 100 - (100 / (1 + rs))
     return rsi
 
-rl['RSI_14'] = calculate_rsi(rl['Close'], 14)
+# rl['RSI_14'] = calculate_rsi(rl['Close'], 14)
 
 def calculate_macd(series, short_period=12, long_period=26, signal_period=9):
     ema_short = series.ewm(span=short_period, adjust=False).mean()
@@ -66,11 +50,31 @@ def calculate_macd(series, short_period=12, long_period=26, signal_period=9):
     histogram = macd_line - signal_line
     return macd_line, signal_line, histogram
 
-rl['MACD'], rl['MACD_Signal'], rl['MACD_Hist'] = calculate_macd(rl['Close'])
+# rl['MACD'], rl['MACD_Signal'], rl['MACD_Hist'] = calculate_macd(rl['Close'])
+
+
+def bollinger_bands():
+    rl["Upper Band"] = rl[f"20_SMA"] + (2 * rl["SD"]) # upper band formula
+    rl["Middle band"] =rl[f"20_SMA"] # middle band formula 
+    rl["Lower Band"] = rl[f"20_SMA"] - (2 * rl["SD"]) # Lower band formula
+
+    rl["percent_b"] =(rl['Close']- rl['Lower Band']) / (rl["Upper Band"]-rl["Lower Band"]) # B percentage (volatility) 
+
+
+def standard_deviation():
+    rl["SD"]= rl["Close"].rolling(window=20).std() # Standard deviation formula
+
+def sma(x):
+    rl[f"{x}_SMA"]= rl["Close"].rolling(window = x).mean() # 20 SMA formula 
+
+    rl["price_vs_sma"]= (rl['Close'] - rl[f"{x}_SMA"])/ rl[f"{x}_SMA"] # sma deviation 
+
+
+def percentage():
+    rl["chng %"] = rl["Close"].pct_change() * 100 # close price percentage change
+    rl["volume %"] =rl["Volume"].pct_change() * 100 # Volume percentage change . 
 
 
 
-
-rl.to_excel("Data/dataset_with_3Class.xlsx", index=False)
 
 
---
+++ manipulation/truecreation.py
@@ -0,0 +1,12 @@
+from creation import * 
+
+create()
+create_class()
+sma(5)
+sma(10)
+sma(20)
+standard_deviation()
+bollinger_bands()
+percentage()
+
+rl.to_excel("Demo.xlsx", index=False)
